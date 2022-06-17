842c272e988d6c741c16c8f5003ed727b2577438
S0k3s <94992699+S0k3s@users.noreply.github.com>
2022-06-17 11:42:02 -0400

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -10,16 +10,18 @@
 
 using namespace std;
 
+// initailized for cursor manipulation
 HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
 
-int unlocks = 2;
+int unlocks = 2; // 2 because the game starts with 2 unlocks
 
-int prices[5]{5, 10, 500, 10000, 1000000}; // per 1 unit
+float prices[6]; // initial prices per 1 unit
 
 string NewGame() {
 	string file; // input for file selection
 	string line;
 	fstream File_Names("File_Names.txt"); // opening file names file
+
 
 	bool check = true;
 
@@ -35,6 +37,7 @@ string NewGame() {
 		}
 		File_Names.close();
 
+
 		if (check == true) {
 			fstream File_Names("File_Names.txt", ios::out | ios::app); // opening file names file
 			File_Names << endl << file; // writing the name of the new file in the file names file for future use
@@ -47,15 +50,15 @@ string NewGame() {
 			Trucks
 			Dinamite
 			Nukes
-			FullPower
+			Laser Sharks
 			*/
 
 			ofstream New_File(file); // creating file with inputed name
 			New_File << "0\n1\n1\n0\n0\n0\n0";
 
-			cout << "Welcome to Mining Mania!\n\n" << "This game requires no skill, just patience and CPU power.\n" << "The aim of the game is to mine the most gold as you possibly can.\n";
+			cout << "Welcome to Mining Mania!\n\n" << "This game requires no skill, just patience.\n" << "The aim of the game is to mine the most gold as you possibly can.\n";
 			cout << "Have fun and just let it run.";
-			Sleep(10000); // wait ten seconds to read
+			Sleep(5000); // wait five seconds to read
 		}
 		else { cout << "File already exists\n"; }
 	} while (check == false);
@@ -68,6 +71,8 @@ string LoadGame() {
 	string file;
 	string line;
 	vector<string> fileList;
+	vector<string> loadGameStats;
+
 
 	fstream File_Names("File_Names.txt"); // oppening the file Names file
 
@@ -78,34 +83,63 @@ string LoadGame() {
 	}
 	else { cout << "Could not access saves\n"; } // if the file couldn't be found
 
+
+	// putting the files stats into a vector
+	for (int i = 0; i < fileList.size(); i++) {
+
+		fstream loadGameStatFile(fileList[i]);
+		getline(loadGameStatFile, line);
+		loadGameStats.push_back(line);
+		loadGameStatFile.close();
+	}
+
+
 	bool LoadGameLoop = true;
 	int select = 0;
 
 	while (LoadGameLoop == true) {
-		
+
+
+		// printing the box with available files inside
+		COORD cursorPos = { 40, 3 };
+		SetConsoleCursorPosition(console, cursorPos);
+		cout << "'_______________'";
+
 		for (int i = 0; i < fileList.size(); i++) { // looping over the lines
+
+			cursorPos = { 40, SHORT(i + 4) };
+			SetConsoleCursorPosition(console, cursorPos);
+
 			if (i == select) {
 				SetConsoleTextAttribute(console, 112);
 			}
-			cout << fileList[i] << endl; // output current line
+			cout << "|" << fileList[i]; 
 			SetConsoleTextAttribute(console, 15);
+			cursorPos = { 56, SHORT(i + 4) };
+			SetConsoleCursorPosition(console, cursorPos);
+			cout << "|  " << "Progress: $" << loadGameStats[i];
 		}
-		
+
+		cursorPos = { 40, SHORT(fileList.size() + 4) };
+		SetConsoleCursorPosition(console, cursorPos);
+		cout << ",---------------,";
+
+		// getting keyboard information
 		if (_kbhit()) {
 
 			int keyPressed = _getch();
 
-			if (keyPressed == 13) {
-				file = line;
+			if (keyPressed == 13) { // Enter
+				file = fileList[select]; // open selected file
 				LoadGameLoop = false;
 			}
-			else if (keyPressed == 72) {
+			else if (keyPressed == 72) { // up
 				if (select == 0) {
 					select = (fileList.size());
 				}
 				select--;
 			}
-			else if (keyPressed == 80) {
+			else if (keyPressed == 80) { // down
 				select++;
 				if (select == (fileList.size())) {
 					select = 0;
@@ -113,16 +147,16 @@ string LoadGame() {
 					
 			}
 		}
-		COORD cursorPos = { 0, 0 };
-		SetConsoleCursorPosition(console, cursorPos);
 		File_Names.seekg(0);
 	}
 	File_Names.close();
 	return file;
 }
 void Print(string OpenFile, int upgradeSelect) {
+
 	array<double, 7>info; // fancy array to pass into another function
 	string line;
+
 
 	fstream File(OpenFile);
 
@@ -138,57 +172,95 @@ void Print(string OpenFile, int upgradeSelect) {
 	}
 	File.close();
 
-	// printing current info
+	// calculating the current price of each upgrade
+	prices[0] = (5 * pow(info[1], 2)); // picklvl
+	prices[1] = (10 * pow(info[2], 2)); // miners
+	prices[2] = (500 * pow(info[3] + 1, 2.5)); // trucks
+	prices[3] = (10000 * pow(info[4] + 1, 2.5)); // dynamite
+	prices[4] = (1000000 * pow(info[5] + 1, 2.5)); // nukes
+	prices[5] = (1000000000 * pow(info[6] + 1, 2.5)); // Laser sharks
 
+
+			// printing current info
+
+	// printing the box first
+	cout << "'_________________________________________'\n";
+	for (int i = 0; i < 20; i++) {
+		COORD cursorPos = { 0, i + 1 };
+		SetConsoleCursorPosition(console, cursorPos);
+		cout << "|";
+		cursorPos = { 42, short(i + 1) };
+		SetConsoleCursorPosition(console, cursorPos);
+		cout << "|";
+	}
+	COORD cursorPos = { 0, 21 };
+	SetConsoleCursorPosition(console, cursorPos);
+	cout << ",-----------------------------------------,";
+
+			// printing the text inside the box
+	cursorPos = { 2, 2 };
+	SetConsoleCursorPosition(console, cursorPos);
 	cout << "Current $: " << info[0] << "\n\n";
+
+	cursorPos = { 2, 4 };
+	SetConsoleCursorPosition(console, cursorPos);
 	if (upgradeSelect == 1) { SetConsoleTextAttribute(console, 112); }
-	cout << "Picklvl: " << info[1] << endl << "Cost: $" << prices[0] << "\n\n";
+	cout << "Picklvl: " << info[1] << ",   Cost: $" << prices[0] << "\n\n";
 	SetConsoleTextAttribute(console, 15);
 
+	cursorPos = { 2, 7 };
+	SetConsoleCursorPosition(console, cursorPos);
 	if (upgradeSelect == 2) { SetConsoleTextAttribute(console, 112); }
-	cout << "Miners: " << info[2] << endl << "Cost: $" << prices[1] << "\n\n";
+	cout << "Miners: " << info[2] << ",   Cost: $" << prices[1] << "\n\n";
 	SetConsoleTextAttribute(console, 15);
 
+	cursorPos = { 2, 10 };
+	SetConsoleCursorPosition(console, cursorPos);
 	if (upgradeSelect == 3) { SetConsoleTextAttribute(console, 112); }
 	if (info[1] < 10) {
 		cout << "Trucks: " << "Locked until picklvl 10\n\n";
 	}
 	else {
-		cout << "Trucks: " << info[3] << endl << "Cost: $" << prices[2] << "\n\n";
+		cout << "Trucks: " << info[3] << ",   Cost: $" << prices[2] << "\n\n";
 		unlocks = 3;
 	}
 	SetConsoleTextAttribute(console, 15);
 
+	cursorPos = { 2, 13 };
+	SetConsoleCursorPosition(console, cursorPos);
 	if (upgradeSelect == 4) { SetConsoleTextAttribute(console, 112); }
 	if (info[1] < 50) {
 		cout << "Dynamite: " << "Locked until picklvl 50\n\n";
 	}
 	else {
-		cout << "Dynamite: " << info[4] << endl << "Cost: $" << prices[3] << "\n\n";
+		cout << "Dynamite: " << info[4] << ",   Cost: $" << prices[3] << "\n\n";
 		unlocks = 4;
 	}
 	SetConsoleTextAttribute(console, 15);
 
+	cursorPos = { 2, 16 };
+	SetConsoleCursorPosition(console, cursorPos);
 	if (upgradeSelect == 5) { SetConsoleTextAttribute(console, 112); }
 	if (info[1] < 100) {
 		cout << "Nukes: " << "Locked until picklvl 100\n\n";
 	}
 	else {
-		cout << "Nukes: " << info[5] << endl << "Cost: $" << prices[4] << "\n\n";
+		cout << "Nukes: " << info[5] << ",   Cost: $" << prices[4] << "\n\n";
 		unlocks = 5;
 	}
 	SetConsoleTextAttribute(console, 15);
 
+	cursorPos = { 2, 19 };
+	SetConsoleCursorPosition(console, cursorPos);
 	if (upgradeSelect == 6) { SetConsoleTextAttribute(console, 112); }
 	if (info[1] < 1000) {
-		cout << "FullPower: " << "Locked until picklvl 1000\n\n";
+		cout << "Laser Sharks: " << "Locked until picklvl 1000\n\n";
 	}
 	else {
-		cout << "FullPower: " << info[6] << endl << "Cost: $" << prices[5] << "\n\n";
+		cout << "Laser Sharks: " << info[6] << ",   Cost: $" << prices[5] << "\n\n";
 		unlocks = 6;
 	}
 	SetConsoleTextAttribute(console, 15);
-
 }
 void Write(string OpenFile, array<double, 7>info) { // function to save to external save file
 	fstream File;
@@ -200,18 +272,22 @@ void Write(string OpenFile, array<double, 7>info) { // function to save to exter
 	File.close();
 }
 void Shop(string OpenFile) {
-	array<double, 7>info; // {Current$, Picklvl, Miners, Trucks, Dinamite, Nukes, FullPower}
+	array<double, 7>info; // {Current$, Picklvl, Miners, Trucks, Dinamite, Nukes, Laser Sharks}
 	string input;
 	string upgrade;
 	int amount;
 	string line;
 	bool shop_loop = true;
 
+
 	while (shop_loop == true) {
 		clear();
 		fstream File(OpenFile);
+
 		int upgradeSelect = 0;
 
+
+		// get file info
 		for (int i = 0; i < 7; i++) {
 			getline(File, line);
 			if (i == 0) { info[i] = stod(line); } // Current$
@@ -220,18 +296,20 @@ void Shop(string OpenFile) {
 			if (i == 3) { info[i] = stod(line); } // Trucks
 			if (i == 4) { info[i] = stod(line); } // Dinamite
 			if (i == 5) { info[i] = stod(line); } // Nukes
-			if (i == 6) { info[i] = stod(line); } // FullPower
+			if (i == 6) { info[i] = stod(line); } // Laser Sharks
 		}
 		File.close();
 
 		Print(OpenFile, upgradeSelect);
 
+
 		bool mainMenuLoop = true;
 		int select = 0;
 		while (mainMenuLoop == true) {
 
-			COORD cursorPos = { 0, 19 };
 
+			// Set up options
+			COORD cursorPos = { 0, 25 };
 			if (select == 0) {
 				SetConsoleCursorPosition(console, cursorPos);
 				cout << "Would you like to ";
@@ -248,15 +326,16 @@ void Shop(string OpenFile) {
 				SetConsoleTextAttribute(console, 15);
 			}
 
+			// check keyboard inputs
 			if (_kbhit()) {
 
 				int keyPressed = _getch();
 				// cout << endl << keyPressed;
 
-				if (keyPressed == 13) {
+				if (keyPressed == 13) { // enter
 					mainMenuLoop = false;
 				}
-				else if (keyPressed == 75 || keyPressed == 77) {
+				else if (keyPressed == 75 || keyPressed == 77) { // if left or right key
 					if (select == 0) {
 						select = 1;
 					}
@@ -266,6 +345,7 @@ void Shop(string OpenFile) {
 				}
 			}
 		}
+
 
 		if (select == 0) { // upgrade screen
 			bool upgrade_loop = true;
@@ -277,21 +357,22 @@ void Shop(string OpenFile) {
 			while (upgradeChoiceLoop == true) {
 				Print(OpenFile, upgradeSelect);
 
+				// checking keyboard inputs
 				if (_kbhit()) {
 
 					int keyPressed = _getch();
 					// cout << keyPressed;
 
-					if (keyPressed == 13) {
+					if (keyPressed == 13) { // enter
 						upgradeChoiceLoop = false;
 					}
-					else if (keyPressed == 72) {
+					else if (keyPressed == 72) { // up
 						if (upgradeSelect == 1) {
 							upgradeSelect = (unlocks + 1);
 						}
 						upgradeSelect--;
 					}
-					else if (keyPressed == 80) {
+					else if (keyPressed == 80) { // down
 						upgradeSelect++;
 						if (upgradeSelect == (unlocks + 1)) {
 							upgradeSelect = 1;
@@ -300,8 +381,11 @@ void Shop(string OpenFile) {
 					}
 				}
 
+
+				COORD cursorPos = { 0, 23 };
+				SetConsoleCursorPosition(console, cursorPos);
 				cout << "What would you like to upgrade? ";
-				COORD cursorPos = { 0, 0 };
+				cursorPos = { 0, 0 };
 				SetConsoleCursorPosition(console, cursorPos);
 			}
 
@@ -310,19 +394,15 @@ void Shop(string OpenFile) {
 			int upgradeAmountLoop = true;
 			select = 0;
 
-			prices[0] = (5 * pow(info[1], 2)); // picklvl
-			prices[1] = (10 * pow(info[2], 2)); // miners
-			prices[2] = (500 * pow(info[3] + 1, 2.5)); // trucks
-			prices[3] = (10000 * pow(info[4] + 1, 2.5)); // dynamite
-			prices[4] = (1000000 * pow(info[5] + 1, 2.5)); // nukes
-			prices[5] = 1000000000; // full power
-
 			while (upgradeAmountLoop == true) {
+
+
+				// printing upgrade amount screen
 				maxUpgrade = (info[0] / prices[upgradeSelect - 1]);
 				if (select == 0) {
 					cout << "How much do you want to upgrade by?\n\n\t\t\t";
 					SetConsoleTextAttribute(console, 112);
-					cout << "Max Amout:" << maxUpgrade;
+					cout << "Max Amount:" << maxUpgrade;
 					SetConsoleTextAttribute(console, 15);
 					cout << "\t\t\tCustom Amount";
 				}
@@ -333,15 +413,16 @@ void Shop(string OpenFile) {
 					SetConsoleTextAttribute(console, 15);
 				}
 
+
 				if (_kbhit()) {
 
 					int keyPressed = _getch();
 					// cout << endl << keyPressed;
 
-					if (keyPressed == 13) {
+					if (keyPressed == 13) { // enter
 						upgradeAmountLoop = false;
 					}
-					else if (keyPressed == 75 || keyPressed == 77) {
+					else if (keyPressed == 75 || keyPressed == 77) { // left or right key
 						if (select == 0) {
 							select = 1;
 						}
@@ -357,6 +438,8 @@ void Shop(string OpenFile) {
 			
 			clear();
 
+
+			// custom upgrade amount
 			if (select == 0) {
 				amount = maxUpgrade;
 			}
@@ -365,6 +448,8 @@ void Shop(string OpenFile) {
 				cin >> amount;
 			}
 
+
+			// make transaction
 			if (upgradeSelect == 1 && (info[0] >= (prices[0] * amount))) { // picklvl
 				info[0] -= (prices[0] * amount);
 				info[1] += amount;
@@ -385,28 +470,44 @@ void Shop(string OpenFile) {
 				info[0] -= (amount * prices[4]);
 				info[5] += amount;
 			}
-			else if (upgradeSelect == 6 && (info[0] >= (prices[5] * amount))) { // full power
+			else if (upgradeSelect == 6 && (info[0] >= (prices[5] * amount))) { // Laser Sharks
 				info[0] -= (amount * prices[5]);
 				info[6] += amount;
 			}
 			else {
 				cout << "\nCould not upgrade\n\n"; // error if incorect input or not enough money
 			}
+
 			fstream File(OpenFile);
 			for (int i = 0; i < 7; i++) {
 				File << info[i] << endl;
 			}
 			File.close();
 		}
+
+
 		else if (select == 1) {
-			double MPS = (1 + (info[1] / 20)) * ((1 + info[2]/10) + (info[3]) + (50 * info[4]) + (1000 * info[5])); // calculating money per second
+
+
+			double MPS = (1 + (info[1] / 20)) * ((1 + info[2]/10) + (info[3]) + (50 * info[4]) + (1000 * info[5]) + (10000 * info[6])); // calculating money per second
 			bool mine_loop = true;
 			int cycles = 0;
+			int frame = 1;
+
 
 			clear();
+			COORD cursorPos = { 10, 12 };
+			SetConsoleCursorPosition(console, cursorPos);
 
-			cout << "			Current Money: $" << info[0] << "			Money per second: " << MPS << endl;
-			cout << "Press any key to return to the upgrade screen";
+			// output screen
+			cout << "		   Current Money: $" << info[0] << "			Money per second: " << MPS << "\n\n";
+			cout << "				   Press any key to return to the upgrade screen";
+
+			// opening art files
+			fstream artFile1("Ascii Art Files\\MiningFrame1.txt");
+			fstream artFile2("Ascii Art Files\\MiningFrame2.txt");
+			fstream artFile3("Ascii Art Files\\MiningFrame3.txt");
+			fstream artFile4("Ascii Art Files\\MiningFrame4.txt");
 
 			while (mine_loop == true) {
 				if (info[6] == 0) {
@@ -416,29 +517,70 @@ void Shop(string OpenFile) {
 					}
 					else { // if not, wait 10 ms
 						Sleep(10);
-					}
-
-					COORD cursorPos = { 40, 0 };
-					SetConsoleCursorPosition(console, cursorPos);
-					info[0] += MPS / 100;
-					cout << info[0];
-					cycles = 0;
-				}
-				else if (info[6] > 0) { // if player has a full power upgrade, take out sleep function
-					if (_kbhit()) {
-						_getch();
-						mine_loop = false;
-					}
-					else {
-						info[0] += ((1 + (info[1] / 20)) * ((info[2]) + (5 * info[3]) + (50 * info[4]) + (1000 * info[5])) * info[6]);
 						cycles++;
 					}
 
-					if (cycles == 10000) {
-						clear();
-						cout << "			Current Money: $" << info[0] << endl;
-						cout << "Press any key to return to the upgrade screen";
+
+					// updating current$
+					COORD cursorPos = { 43, 12 };
+					SetConsoleCursorPosition(console, cursorPos);
+					info[0] += MPS / 100;
+					cout << info[0];
+
+					cursorPos = { 75 , 9 };
+					SetConsoleCursorPosition(console, cursorPos);
+
+					cursorPos = { 0 , 2 };
+					SetConsoleCursorPosition(console, cursorPos);
+
+
+					// move forward a frame
+					if (cycles == 5) {
+						frame++;
 						cycles = 0;
+					}
+					if (frame == 5) {
+						frame = 1;
+					}
+
+
+					// print art
+					if (frame == 1) {
+						while (getline(artFile1, line)) {
+							cout << "\t\t\t\t\t" << line << endl;
+						}
+						artFile1.clear();
+						artFile1.seekg(0);
+					}
+					else if (frame == 2) {
+						while (getline(artFile2, line)) {
+							cout << "\t\t\t\t\t" << line << endl;
+						}
+						artFile2.clear();
+						artFile2.seekg(0);
+
+					}
+					else if (frame == 3) {
+						while (getline(artFile3, line)) {
+							cout << "\t\t\t\t\t" << line << endl;
+						}
+						artFile3.clear();
+						artFile3.seekg(0);
+
+					}
+					else if (frame == 4) {
+						while (getline(artFile4, line)) {
+							cout << "\t\t\t\t\t" << line << endl;
+						}
+						artFile4.clear();
+						artFile4.seekg(0);
+					}
+
+					if (mine_loop == false) {
+						artFile1.close();
+						artFile2.close();
+						artFile3.close();
+						artFile4.close();
 					}
 				}
 			}
@@ -471,11 +613,13 @@ int main() {
 		COORD cursorPos = { 0, 2 };
 		SetConsoleCursorPosition(console, cursorPos);
 
+		// open art files
 		fstream artFile1("Ascii Art Files\\TitleScreenFrame1.txt");
 		fstream artFile2("Ascii Art Files\\TitleScreenFrame2.txt");
 		fstream artFile3("Ascii Art Files\\TitleScreenFrame3.txt");
 		fstream artFile4("Ascii Art Files\\TitleScreenFrame4.txt");
 
+		// move forward a frame
 		if (cycles == 2) {
 			frame++;
 			cycles = 0;
@@ -483,6 +627,9 @@ int main() {
 		if (frame == 5) {
 			frame = 1;
 		}
+
+
+		// print art
 		if (frame == 1) {
 			while (getline(artFile1, line)) {
 				cout << "\t" << line << endl;
